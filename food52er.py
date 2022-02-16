8ca85f321bd869126e5f1b478d0c399ea4947681
HamdiAAA <hamdi_mohamed2018@hotmail.com>
2022-02-16 10:15:29 -0400

    added endpoint for vedios from yt for the moment

--- food52er.py
+++ food52er.py
@@ -5,11 +5,11 @@ def search_food52(keyword):
     keyword = keyword.replace(' ', '+')
     pattern = r'/recipes/([0-9]{3,5})-'
     url = f"https://food52.com/recipes/search?q={keyword}"
-   
+
     req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
     html = urllib.request.urlopen(req)
     html = html.read().decode()
     ids = list(re.findall(pattern, html))
     
     links = [f"https://food52.com/recipes/{id}" for id in ids[:-1]]
-    return links[:15]
+    return links[:6]
